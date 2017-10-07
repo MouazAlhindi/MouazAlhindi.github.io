c91905598703606800300f3ee4501420a222d5d1
Mouaz Alhindi <mozalhindi@gmail.com>
2017-10-07 00:51:08 -0400

    Routing to html files

---
+++ app.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    
+    /* The following code is the router function. 
+    --------------------------------------------*/
+    function router($route_on){
+      var routers = $($route_on + '[data-function="router"]');
+  
+      routers.each(function(){
+  
+        var $this = $(this);
+        var links  = $this.find('a');
+        var route = $this.attr('data-route');
+  
+        links.each(function(){
+            console.log("Method 2 exec");
+          var link = $(this);
+          link.attr('data-route',route);
+  
+          var route_url = $(this).attr('href');
+  
+          if("[data-route="+route+"]"){
+            link.attr('href',route+'/'+route_url)
+            link.attr('text-route', route+'/'+route_url)
+          }
+  
+        });
+      });
+    }
+    
+    
+    
+    
+    
+    
+    /* Deployment
+    -------------------------------------------*/
+    
+    router('');
+    
+    // router() = return nothing;
+    // router('') => [data-function="router"];
+    // router('div') => div[data-function="router"];
+    // router('section') => section[data-function="router"];
+  
+  })
